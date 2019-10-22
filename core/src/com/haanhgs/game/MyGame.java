d2143e40fedae770994f06cde9938448d0635492
animevn <haanhth21@gmail.com>
2019-10-22 21:38:32 +0700

    move the circle around

--- core/src/com/haanhgs/game/MyGame.java
+++ core/src/com/haanhgs/game/MyGame.java
@@ -2,32 +2,56 @@ package com.haanhgs.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private OrthographicCamera cam;
+	private Vector2 position;
+	private ShapeRenderer renderer;
+	private static float width;
+	private static float height;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, width, height);
+		position = new Vector2(width/20,height - width/20);
+		renderer = new ShapeRenderer();
+
 	}
+
+
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		cam.update();
+		if (position.y >= width/20 && position.x == width/20) {
+			position.y -= 10;
+		}else if (position.y <= width/20 && position.x <= width - width/20){
+			position.x += 10;
+		}else if (position.x >= width - width/20 && position.y <= height - width/20){
+			position.y += 10;
+		}else if (position.y >= height -width/20 && position.x >= width/20){
+			position.x -= 10;
+		}
+
+		renderer.begin(ShapeRenderer.ShapeType.Filled);
+		renderer.setColor(Color.RED);
+		renderer.circle(position.x, position.y, width/20);
+		renderer.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		renderer.dispose();
 	}
 }
